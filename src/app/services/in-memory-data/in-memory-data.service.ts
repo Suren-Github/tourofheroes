3452733e81187f3a2f0d419538349df69bfcd0f6
Surendhar <surendhar.sundaram@pearson.com>
2018-09-05 11:26:01 +0530

    Added in memory data file to the service

---
+++ src/app/services/in-memory-data/in-memory-data.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+@Injectable({
+  providedIn: 'root'
+})
+//export class InMemoryDataService implements InMemoryDbService {
+export class InMemoryDataService {
+
+  // createDb() {
+  //   const heroes = [
+  //     { id: 11, name: 'Luna' },
+  //     { id: 12, name: 'Dragon Knight' },
+  //     { id: 13, name: 'Alchemist' },
+  //     { id: 14, name: 'Storm Spirit' },
+  //     { id: 15, name: 'Juggernaut' },
+  //     { id: 16, name: 'Riki' },
+  //     { id: 17, name: 'Faceless Void' },
+  //     { id: 18, name: 'IQ' },
+  //     { id: 19, name: 'Enigma' },
+  //     { id: 20, name: 'Sand King' }
+  //   ];
+  // }
+}
